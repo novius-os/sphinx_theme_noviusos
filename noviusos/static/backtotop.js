0e695f83202f731a6cc47aac088c3c1268a645c5
Gilles FELIX <felix@novius.com>
2014-01-24 14:32:40 +0100

    Bugfix theme

--- noviusos/static/backtotop.js
+++ noviusos/static/backtotop.js
@@ -1,43 +1,43 @@
-(function($) {
-    $(document).ready(function() {
-        // Back 2 top
-        init_back2top();
+$(document).ready(function() {
+    // Back 2 top
+    init_back2top();
 
-        $('.related select').change(function() {
-            document.location.href = $(this).find(':selected').val();
-        });
-
-        $("[data-toggle='rst-current-version']").click(function() {
-            $("[data-toggle='rst-versions']").toggleClass("shift");
-        });
+    $('.related select').change(function() {
+        document.location.href = $(this).find(':selected').val();
     });
-    function init_back2top() {
-        var $back2top = $('<div><div></div></div>').css({
-            opacity     : 0
-        }).bind('click', function() {
-                $('html, body').animate({
-                    scrollTop: 0
-                }, 400);
-            }).addClass('back2top').hide();
+});
+function init_back2top() {
+    var $back2top = $('<div><div></div></div>').css({
+        opacity     : 0
+    }).bind('click', function() {
+            $('html, body').animate({
+                scrollTop: 0
+            }, 400);
+        }).addClass('back2top').hide();
 
-        $('body').append($back2top);
+    $('body').append($back2top);
 
-        $(window).bind('scroll', function() {
+    $(window).bind('scroll', function() {
 
-            // offset bottom de la page
-            var v_bottom = $(document).scrollTop() + $(window).height();
+        // offset bottom de la page
+        var v_bottom = $(document).scrollTop() + $(window).height();
 
-            // offset bottom du menu
-            var m_bottom = $('.sphinxsidebar').offset().top + $('.sphinxsidebar').height();
+        // offset bottom du menu
+        var m_bottom = $('.sphinxsidebar').offset().top + $('.sphinxsidebar').height();
 
-            if (v_bottom - m_bottom > 200 + $back2top.height()) {
-                // Affiche
-                $back2top.stop().show().fadeTo(200, 1);
-            } else {
-                // Cache
-                $back2top.stop().fadeTo(200, 0).hide();
-            }
-        });
-        $(window).trigger('scroll');
-    }
-}($19));
+        if (v_bottom - m_bottom > 200 + $back2top.height()) {
+            // Affiche
+            $back2top.stop().show().fadeTo(200, 1);
+        } else {
+            // Cache
+            $back2top.stop().fadeTo(200, 0).hide();
+        }
+    });
+    $(window).trigger('scroll');
+}
+
+if (typeof READTHEDOCS_DATA != 'undefined') {
+    $('.related select option[value="' + READTHEDOCS_DATA.version + '"]').each(function() {
+        this.selected = true;
+    });
+}
